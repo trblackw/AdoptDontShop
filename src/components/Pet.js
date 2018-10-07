ba61f43707f6811e3dfef7aa845fd4d7cfe8f699
Tucker <trblackw@ncsu.edu>
2018-10-07 18:50:29 -0400

    altered pet component stylings

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -4,18 +4,7 @@ import styled from "styled-components";
 
 class Pet extends Component {
   render() {
-    const {
-      name,
-      animal,
-      breed,
-      media,
-      location,
-      id,
-      sex,
-      size,
-      description,
-      shelterEmail
-    } = this.props;
+    const { name, animal, media, location, id } = this.props;
 
     let photos = [];
     if (media && media.photos && media.photos.photo) {
@@ -27,20 +16,21 @@ class Pet extends Component {
       profileImage = photos[0].value;
     }
     return (
-      <Link to={`details/${id}`}>
-        <ImageContainer>
-          <img src={profileImage} alt={name} />
-        </ImageContainer>
-        <PetDetails>
-          <h1>{name}</h1>
-          <h3>{`${animal} -- ${location}`}</h3>
-        </PetDetails>
-      </Link>
+        <Link to={`details/${id}`}>
+          <ImageContainer>
+            <img src={profileImage} alt={name} />
+          </ImageContainer>
+          <PetDetails>
+            <h2>{name}</h2>
+            <h4>{location}</h4>
+          </PetDetails>
+        </Link>
     );
   }
 }
 
 export default Pet;
+
 
 export const ImageContainer = styled.div`
   width: 100px;
@@ -59,4 +49,8 @@ const PetDetails = styled.div`
   margin: 0 auto;
   padding: 1em;
   text-align: center;
+  h2,
+  h4 {
+    font-family: "Varela Round", sans-serif;
+  }
 `;
