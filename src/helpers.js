9c80b955fe6ff4d2f924060596a55aabb016e973
Tucker <trblackw@ncsu.edu>
2018-10-07 15:50:17 -0400

    included petfinder api init function to extract out of each component

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,15 @@
+import pf from "petfinder-client";
+
+export const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 export const getPhotos = media => {
-   let photos = [];
- 
-   if (media && media.photos && media.photos.photo) {
-     photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
-   }
-   return { photos };
- };
+  let photos = [];
+
+  if (media && media.photos && media.photos.photo) {
+    photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+  }
+  return { photos };
+};
