4e3be9bb6841c2c428df3ec0eb5a96aaa4a8688d
Tucker <trblackw@ncsu.edu>
2018-10-22 18:23:01 -0400

    removed redux files from context branch

--- src/reducers/animal.js
+++
@@ -1,9 +0,0 @@
-export default function animalReducer(state = "", action) {
-  const { type, payload } = action;
-  switch (type) {
-    case "SET_ANIMAL":
-      return payload;
-    default:
-      return state;
-  }
-}
--- src/reducers/breeds.js
+++
@@ -1,9 +0,0 @@
-export default function breedsReducer(state = "", action) {
-  const { type, payload } = action;
-  switch (type) {
-    case "SET_BREEDS":
-      return payload;
-    default:
-      return state;
-  }
-}
