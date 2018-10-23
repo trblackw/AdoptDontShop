57b542037644b7cb93b4b7132a4da927259b04c7
Tucker <trblackw@ncsu.edu>
2018-10-23 12:53:56 -0400

    modified store config to return a configureStore function

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import reducer from "./reducers";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
 
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(thunk),
-    composeWithDevTools
-  )
-);
+const middleware = [thunk, logger];
+
+export default function configureStore() {
+   return createStore(
+     rootReducer,
+     composeWithDevTools(applyMiddleware(...middleware))
+   );
+ }
+ 
