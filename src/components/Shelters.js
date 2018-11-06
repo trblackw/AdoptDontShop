54223ca6cbda745d35366460e830f5f7484ac0f8
Tucker <trblackw@ncsu.edu>
2018-11-06 10:30:48 -0500

    incorporated toggle component and additional styles for shelters list

--- src/components/Shelters.js
+++ src/components/Shelters.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { petfinder, getLocation, getPlaceDetails } from "../helpers";
 import styled from "react-emotion";
+import { Consumer } from "./SearchContext";
+import { Button } from "./Search";
+import Toggle from "./Toggle";
 
-export default class Shelters extends Component {
+class Shelters extends Component {
   state = {
     shelters: []
   };
   componentDidMount = () => {
+    const { location } = this.props.searchParams;
     petfinder.shelter
       .find({
-        location: "Raleigh, NC",
+        location,
         output: "full"
       })
       .then(res => this.setState({ shelters: res.petfinder.shelters }));
@@ -17,26 +21,69 @@ export default class Shelters extends Component {
 
   render() {
     const { shelter: shelters } = this.state.shelters;
+    const {
+      location: searchLocation,
+      handleLocationChange
+    } = this.props.searchParams;
+
     if (shelters && shelters.length > 0) {
       const coords = shelters.map(item => ({
         lat: item.latitude,
         lng: item.longitude,
         name: item.name
       }));
-       const locationData = coords.map(location => {
-          getLocation(location.lat, location.lng);
-       });
+      const test = coords.map(location =>
+        getLocation(location.lat, location.lng)
+      );
     }
-   //  console.log(shelters);
+
     return (
       <SheltersContainer className="drop-shadow">
-        <SheltersList>
-          {shelters &&
-            shelters.map(shelter => <li key={shelter.id}>{shelter.name}</li>)}
-        </SheltersList>
+        <div className="shelters shadow">
+          <Toggle>
+            {({ toggle, on }) => (
+              <Fragment>
+                <Button
+                  onClick={toggle}
+                  style={{ width: "auto", marginBottom: 0 }}
+                >
+                  Modify search
+                </Button>
+                {on && (
+                  <Fragment>
+                    <input
+                      type="text"
+                      placeholder="New Location"
+                      onChange={handleLocationChange}
+                    />
+                    <button>Go!</button>
+                  </Fragment>
+                )}
+              </Fragment>
+            )}
+          </Toggle>
+          <SheltersList>
+            <small>{searchLocation}</small>
+            {shelters &&
+              shelters.map(shelter => (
+                <Fragment>
+                  <li key={shelter.id}>{shelter.name}</li>
+                  <hr />
+                </Fragment>
+              ))}
+          </SheltersList>
+        </div>
       </SheltersContainer>
     );
   }
+}
+
+export default function SheltersWithContext(props) {
+  return (
+    <Consumer>
+      {context => <Shelters {...props} searchParams={context} />}
+    </Consumer>
+  );
 }
 
 const SheltersContainer = styled("div")`
@@ -45,15 +92,47 @@ const SheltersContainer = styled("div")`
   background: #66b9bf;
   width: 65%;
   height: auto;
+  border-radius: 4px;
+  text-align: center;
+
+  input {
+    padding: 0.5em 0.7em;
+    margin: 1em auto;
+    width: 60%;
+    border-radius: 5px;
+  }
+
+  div.shelters {
+    background: whitesmoke;
+    padding: 1em;
+    margin: 1.5em;
+    border-radius: 5px;
+  }
 `;
 
 const SheltersList = styled("ul")`
   list-style: none;
   padding: 1.5em;
-  margin: 1.5em auto;
+  margin: 0 auto;
+  color: black;
+
+  small {
+    color: #07889b;
+    font-weight: bold;
+  }
+
+  li:nth-child(n + 2) {
+    margin: 1em auto;
+  }
 
   li {
-    margin: 1em auto;
     font-size: 1.5em;
+    margin-bottom: 1em;
+  }
+
+  hr {
+    /* height: 1px; */
+    border: 0;
+    border-top: 1px solid #66b9bf;
   }
 `;
