cea0d212ef8d1639b2d764c683be52f449b7b0d4
Tucker <trblackw@ncsu.edu>
2018-11-13 10:57:26 -0500

    created base express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`app runnin on port ${port}`);
+});
