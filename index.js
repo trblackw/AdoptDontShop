871685d7f00e60af1b704b46cc3be59a689f77c0
Tucker <trblackw@ncsu.edu>
2018-11-18 13:05:22 -0500

    forgot to serve a root file for express server

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
 app.use(express.static(path.join(__dirname, "dist")));
 
 const port = process.env.PORT || 5000;
