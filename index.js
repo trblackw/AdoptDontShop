ac0eb1da2d720abb10fb001abb696bfd5031b756
Tucker <trblackw@ncsu.edu>
2018-11-18 13:08:07 -0500

    corrected file path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "src/index.html"));
 });
 
 app.use(express.static(path.join(__dirname, "dist")));
