c43f36ff0515ead42f54d41130df6541ee5c42a7
Tucker <trblackw@ncsu.edu>
2018-11-21 18:42:14 -0500

    commented out improperly served file

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "src/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname + "src/index.html"));
+// });
 
 app.use(express.static(path.join(__dirname, "dist")));
 
